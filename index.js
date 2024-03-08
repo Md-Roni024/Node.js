21e29a0ef186c913a93ca2584a99c0dde453b7e9
Roni Hossain <roni.cse024@gmail.com>
2024-03-08 08:09:28 +0600

    Lesson3

--- index.js
+++ index.js
@@ -1,48 +1,26 @@
-const fs = require('fs')
-
-//write a file
-fs.writeFile('demo1.txt','My Name is Roni',(err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('Successful.')
-    }
-})
-
-//Read a file
-// fs.readFile('demo1.txt','utf-8',(err,data)=>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log(data)
-//     }
-// })
-
-//Append a file
-// fs.appendFile('demo1.txt',' I am 24 Years old.',(err)=>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Successful.')
-//     }
-// })
+// const os = require('os')
+// //hostName
+// console.log(os.hostname())
+// //homeDir
+// console.log(os.homedir())
+// //freeMem
+// console.log(os.freemem())
+// //totalMem
+// console.log(os.totalmem())
 
 
-//Rename File
-// fs.rename('demo1.txt',' demo2.txt',(err)=>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Successful.')
-//     }
-// })
+const rootPath = (__dirname);
+const filePath = (__filename);
+console.log(rootPath)
+console.log(filePath)
 
 
-//Delete a file
-fs.unlink(' demo2.txt',(err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('Successful.')
-    }
-})
+const path = require('path')
+// console.log(path)
+//Extension Name
+extensionName = path.extname("index.js");
+console.log(extensionName);
+
+//Join
+extJoin = path.join(__dirname + '/api/books')
+console.log(extJoin)
