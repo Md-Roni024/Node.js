e4efc9b8a6adfc7a6a28104973aeda089e109d68
Roni Hossain <roni.cse024@gmail.com>
2024-03-08 08:13:51 +0600

    Lesson6

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const randomm = require('random-fruits-name')
-const moviesNames = require('movies-names')
-console.log(moviesNames.random())
+const http = require('http')
+const port =3000
+const hostname = '127.0.0.1'
+const fs = require('fs');
+
+const myServer = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        fs.readFile("index.html",(err,data)=>{
+            res.writeHead(200,{"Content-Type":"text/html"})
+            res.write(data)
+            res.end();
+        })
+    }
+    else if(req.url === '/about'){
+        fs.readFile("about.html",(err,data)=>{
+            res.writeHead(200,{"Content-Type":"text/html"})
+            res.write(data)
+            res.end();
+        })
+    }
+    else if(req.url === '/contact'){
+        fs.readFile("contact.html",(err,data)=>{
+            res.writeHead(200,{"Content-Type":"text/html"})
+            res.write(data)
+            res.end();
+        })
+    }
+})
+
+myServer.listen(port,hostname,()=>{
+    console.log(`Server is Runnning at http://${hostname}:${port}`)
+})
