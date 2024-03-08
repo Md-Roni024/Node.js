89ad4d7dc871e9a7325c7ca8e9c0f7dcec1e9bc1
Roni Hossain <roni.cse024@gmail.com>
2024-03-08 08:08:07 +0600

    Lesson2

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-const s1 = require('./student')
-// console.log(s1.getName())
-// console.log(s1.getAge())
-console.log(s1.phone)
+const fs = require('fs')
+
+//write a file
+fs.writeFile('demo1.txt','My Name is Roni',(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Successful.')
+    }
+})
+
+//Read a file
+// fs.readFile('demo1.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(data)
+//     }
+// })
+
+//Append a file
+// fs.appendFile('demo1.txt',' I am 24 Years old.',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Successful.')
+//     }
+// })
+
+
+//Rename File
+// fs.rename('demo1.txt',' demo2.txt',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Successful.')
+//     }
+// })
+
+
+//Delete a file
+fs.unlink(' demo2.txt',(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Successful.')
+    }
+})
